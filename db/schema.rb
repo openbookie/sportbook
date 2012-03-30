adff7e8baa71ff495dc372913d747b2cc8fda6ca
Gerald Bauer <gerald.bauer@gmail.com>
2012-03-30 19:12:45 +0200

    add bundesliga and europa league

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120329002301) do
+ActiveRecord::Schema.define(:version => 20120330163625) do
 
   create_table "days", :force => true do |t|
     t.integer  "event_id",   :null => false
