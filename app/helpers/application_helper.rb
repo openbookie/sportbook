62747e86921c36c565ac2b7f434dbb9c99460e3e
Gerald Bauer <gerald.bauer@gmail.com>
2012-05-23 19:51:08 +0200

    add some more about info

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,16 @@
 
 module ApplicationHelper
 
+  def powered_by
+    ## todo/fix: use version from wettpool module
+    content_tag :div do
+      link_to( 'Wettpool/1', 'http://geraldb.github.com/wettpool' )                       + ' - ' +
+      content_tag( :span, "Ruby/#{RUBY_VERSION} (#{RUBY_RELEASE_DATE}/#{RUBY_PLATFORM}) on") + ' ' +
+      content_tag( :span, "Rails/#{Rails.version} (#{Rails.env})" )                       + ' - ' +
+      content_tag( :span, "#{request.headers['SERVER_SOFTWARE'] || request.headers['SERVER']}" )
+    end
+  end
+
   def title(*parts)
     unless parts.empty?
       content_for :title do
