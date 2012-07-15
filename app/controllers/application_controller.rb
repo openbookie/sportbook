082ea4be7b949d26681a4b577b5a5c247deb411c
Gerald Bauer <gerald.bauer@gmail.com>
2012-07-15 20:48:18 +0200

    start adding string tables for locales

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
-  include ApplicationHelper  
-end
+  include ApplicationHelper
+  
+  before_filter :set_locale
+ 
+  def set_locale
+    session[:locale] = I18n.locale = params[:locale] || session[:locale] || I18n.default_locale
+    
+    ## todo:
+    ## use request.env['HTTP_ACCEPT_LANGUAGE'] to set default locale
+    ##   on startup
+    
+  end
+  
+end # class ApplicationController
---
+++ config/initializers/i18n.rb
@@ -0,0 +1,10 @@
+# encoding: utf-8
+
+I18n.default_locale = :en
+
+LANGUAGES = [
+  ['English', 'en'],
+  ['Deutsch', 'de'],
+  ['Español', 'es'],
+  ['Română',  'ro']
+]
