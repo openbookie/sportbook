9001ffaad155351a5dd68affd20cd41a6bb4fffa
Gerald Bauer <gerald.bauer@gmail.com>
2012-09-16 16:41:35 +0200

    add cl 2012-13 teams

--- db/seeds/cl/teams.rb
+++ db/seeds/cl/teams.rb
@@ -3,22 +3,57 @@
 puts "*** loading seed data in seeds/cl/teams.rb"
 
 teams = [
+  [ 'porto',        'FC Porto',             'cl/porto.png' ],
+  [ 'kiew',         'Dynamo Kiew',          'cl/kiew.png' ],
+  [ 'paris',        'Paris St-Germain',     'cl/paris.png' ],
+  [ 'zagreb',       'Dinamo Zagreb',        'cl/zagreb.png' ],
+
+  [ 'arsenal',      'FC Arsenal London',    'cl/arsenal.png' ],
+  [ 'schalke',      'FC Schalke 04',        'cl/schalke.png' ],
+  [ 'olympiacos',   'Olympiakos Piräus',    'cl/olympiacos.png' ],
+  [ 'montpellier',  'HSC Montpellier',      'cl/montpellier.png' ],
+
+  [ 'milan',        'AC Mailand',           'cl/milan.png' ],
+  [ 'zenit',        'Zenit St. Petersburg', 'cl/zenit.png' ],
+  [ 'anderlecht',   'RSC Anderlecht',       'cl/anderlecht.png' ],
+  [ 'malaga',       'FC Málaga',            'cl/malaga.png' ],
+
+  [ 'madrid',       'Real Madrid',          'cl/madrid.png' ],
+  [ 'mancity',      'Manchester City',      'cl/mancity.png' ],
+  [ 'ajax',         'Ajax Amsterdam',       'cl/ajax.png' ],
+  [ 'dortmund',     'Borussia Dortmund',    'cl/dortmund.png' ],
+
+  [ 'chelsea',      'FC Chelsea',           'cl/chelsea.png' ],
+  [ 'donezk',       'Schachtjor Donezk',    'cl/donezk.png' ],
+  [ 'juventus',     'Juventus Turin',       'cl/juventus.png' ],
+  [ 'nordsjalland', 'FC Nordsjælland',      'cl/nordsjalland.png' ],
+
+  [ 'bayern',    'FC Bayern München',       'cl/bayern.png' ],
+  [ 'valencia',  'FC Valencia',             'cl/valencia.png' ],
+  [ 'lille',     'OSC Lille',               'cl/lille.png' ],
+  [ 'borissow',  'BATE Borissow',           'cl/borissow.png' ],
+
+  [ 'barcelona', 'FC Barcelona',             'cl/barcelona.png' ],
+  [ 'benfica',   'Benfica Lissabon',         'cl/benfica.png' ],
+  [ 'spartak',   'Spartak Moskau',           'cl/spartak.png' ],
+  [ 'celtic',    'Celtic Glasgow',           'cl/celtic.png' ],
+
+  [ 'manunited',   'Manchester United',      'cl/manunited.png' ],
+  [ 'braga',       'Sporting Braga',         'cl/braga.png' ],
+  [ 'galatasaray', 'Galatasaray',            'cl/galatasaray.png' ],
+  [ 'cluj',        'CFR Cluj',               'cl/cluj.png' ],
+
+###################
+# more teams from cl/el 2011-12
+#
   [ 'inter',     'FC Internazionale Milano',  'cl/internazionale.png' ],
   [ 'marseille', 'Olympique Marseille',       'cl/marseille.png' ],
-  [ 'bayern',    'FC Bayern München',         'cl/bayern.png' ],
   [ 'basel',     'FC Basel 1893',             'cl/basel.png' ],
-  [ 'chelsea',   'Chelsea FC',                'cl/chelsea.png' ],
   [ 'napoli',    'SSC Napoli',                'cl/napoli.png' ],
-  [ 'madrid',    'Real Madrid CF',            'cl/madrid.png' ],
   [ 'moskva',    'PFC CSKA Moskva',           'cl/moskva.png' ],
-  [ 'milan',     'AC Milan',                  'cl/milan.png' ],
-  [ 'benfica',   'Benfica Lissabon',          'cl/benfica.png' ],
-  [ 'barcelona', 'FC Barcelona',              'cl/barcelona.png' ],
   [ 'apoel',     'APOEL Nikosia',             'cl/apoel.png' ],
 
   [ 'az',        'AZ Alkmaar',           'cl/az.png' ],
-  [ 'valencia',  'Valencia CF',          'cl/valencia.png' ],
-  [ 'schalke',   'FC Schalke 04',        'cl/schalke.png' ],
   [ 'athletic',  'Athletic Bilbao',      'cl/athletic.png' ],
   [ 'sporting',  'Sporting Lisboa',      'cl/sporting.png' ],
   [ 'metalist',  'FC Metalist Kharkiv',  'cl/metalist.png' ],
