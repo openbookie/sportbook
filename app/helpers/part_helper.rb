ea3d6edd72849f7929ab4e7c3492fe9c0721e2f3
Gerald Bauer <gerald.bauer@gmail.com>
2012-10-03 12:05:14 +0200

    more parts for reuse

--- app/helpers/part_helper.rb
+++ app/helpers/part_helper.rb
@@ -19,6 +19,18 @@ module PartHelper
                         :pool => play.pool
                       }
   end
+  
+  def render_play_team1( play, opts={} )
+    render :partial => 'shared/td_play_team1', :locals => { :play => play }
+  end
+  
+  def render_play_team2( play, opts={} )
+    render :partial => 'shared/td_play_team2', :locals => { :play => play }
+  end
+
+  def render_play_team3( play, opts={} )
+    render :partial => 'shared/td_play_team3', :locals => { :play => play }
+  end
 
   def render_play_teams_for_form( play, form, opts={} )
     
@@ -33,6 +45,10 @@ module PartHelper
 
   def render_game_date( game, opts={} )
     render :partial => 'shared/td_game_date', :locals => { :game => game }
+  end
+
+  def render_game_toto12x( game, opts={} )
+    render :partial => 'shared/td_game_toto12x', :locals => { :game => game }
   end
 
   def render_game_score( game, opts={} )
@@ -60,13 +76,24 @@ module PartHelper
   def render_tip_score( tip, opts={} )
     render :partial => 'shared/td_tip_score', :locals => { :tip  => tip }
   end
+  
+  def render_tip_score_v2( tip, opts={} )
+    show_tips = opts[:show_tips].present? ? opts[:show_tips] : true  # default true
+    show_pts  = opts[:show_pts].present? ? opts[:show_pts] : false  # default false
 
+    render :partial => 'shared/td_tip_score_v2',
+           :locals => { :tip       => tip,
+                        :game      => tip.game,
+                        :show_tips => show_tips,
+                        :show_pts  => show_pts }
+  end
+  
   def render_tip_toto12x( tip, opts={} )
     render :partial => 'shared/td_tip_toto12x', :locals => { :tip  => tip }
   end
 
   def render_tip_score_input_for_form( tip, form, opts={} )
-     tabindex = opts[:tabindex] ||= 1000
+     tabindex = opts[:tabindex] || 1000
      
      render :partial => 'shared/td_tip_score_input',
             :locals  => { :tip      => tip,
