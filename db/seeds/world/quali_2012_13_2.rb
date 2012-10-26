65f0845c71f11d8903cf348c41ddaef51e143404
Gerald Bauer <gerald.bauer@gmail.com>
2012-10-26 16:43:23 +0200

    add more quotes

--- db/seeds/world/quali_2012_13_2.rb
+++ db/seeds/world/quali_2012_13_2.rb
@@ -13,21 +13,10 @@ swe = Team.find_by_key!( 'swe' )
 
 world = Event.find_by_key!( 'wmql' )
 
-world2 = Round.find_by_event_id_and_pos!( world.id, 2 )
 
 tipp3     = Service.find_by_key!( 'tipp3' )
 betathome = Service.find_by_key!( 'betathome' )
 
-
-world2_betathome_odds = [
- [ irl, ger, ],
- [ fro, swe, ],
- [ kaz, aut, ],
- [ ger, swe, ],
- [ fro, irl, ],
- [ aut, kaz, ]]
-
-Quote.create_from_ary_for_round!( world2_betathome_odds, betathome, world2 )
 
 
 
