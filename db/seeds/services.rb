bf346a9b21f06e695c1ac268ab836440db91d56a
Gerald Bauer <gerald.bauer@gmail.com>
2012-11-12 11:35:25 +0100

    move quotes to sport.db market

--- db/seeds/services.rb
+++
@@ -1,8 +0,0 @@
-# encoding: utf-8
-
-Service.create!( :key => 'tipp3',     :title => 'tipp3' )  # tipp3 classic
-Service.create!( :key => 'tipico',    :title => 'tipico' )
-Service.create!( :key => 'betathome', :title => 'bet-at-home' )
-
-## todo: use new version constant for app module e.g. Wettpool::VERSION ??
-Prop.create!( :key => 'db.seeds.services.version', :value => '1' )
