8bf479f2f36275295aff0b1d8169d90e3051e382
Gerald Bauer <gerald.bauer@gmail.com>
2014-02-17 10:29:21 -0600

    add fx debug log

--- app/controllers/admin/fixtures_controller.rb
+++ app/controllers/admin/fixtures_controller.rb
@@ -4,11 +4,14 @@ class Admin::FixturesController < Admin::BaseController
 
   def index
   end
-  
+
   def create
     event = Event.find( params[:event_id] )
     text  = params[:text]
-    
+
+    puts "[FixturesController] event_id=#{params[:event_id]}, event.key=#{event.key}"
+    puts "   text=#{text}"
+
     reader = SportDB::Reader.new
     reader.load_fixtures_from_string( event.key, text )
     
